d12c00658df8b459b6cff7b44b86b72906fa77a0
Ibr4h3m-k4m <brahim.kamraoui@gmail.com>
2025-12-08 18:52:58 +0100

    Updated-The-Logic-To-Use-shared-function

--- routes/weather.py
+++ routes/weather.py
@@ -6,13 +6,14 @@ from models.weather import WeatherResponse
 
 weather_router = APIRouter(prefix="/weather", tags=["weather"])
 
+
+# Route 1: Get by City Name
 @weather_router.get("/{city}" , response_model=WeatherResponse)
 async def get_weather(city: str):
     data = await get_weather_by_city(city)
     return data
 
 # Route 2: Get by Coordinates
-# Example: GET /weather/direct?lat=51.5&lon=-0.12
 @weather_router.get("/weather/direct", response_model=WeatherResponse)
 async def read_weather_by_coords(
     lat: float = Query(..., description="Latitude", ge=-90, le=90),
--- services/weather_client.py
+++ services/weather_client.py
@@ -44,69 +44,10 @@ async def get_weather_by_city(city_name: str):
 
         location = geo_data["results"][0]
         
-        # 2. REUSE: Call the shared function
+        # 2. REUSE: Call the shared function to not implement the logic twice and be redundant
         return await _fetch_weather_raw(
             client, 
             location["latitude"], 
             location["longitude"], 
             location["name"]
         )
-
-
-
-
-
-"""async def get_weather_data(city : str):
-    async with httpx.AsyncClient(timeout=HTTP_TIMEOUT,headers=DEFAULT_HEADERS) as client:
-        # first step geocoding to properly use the api 
-        # Note: We pass the dict to 'params', letting httpx handle the URL formatting
-        geo_params = {
-            "name": city, 
-            "count": 1, 
-            "language": "en", 
-            "format": "json"
-        }
-        geo_res = await client.get(GEO_BASE_URL, params= geo_params)
-        geo_data = geo_res.json()
-        
-        if not geo_data:
-            return HTTPException(status_code=404, detail=f'City Not found')
-        location = geo_data["results"][0]
-        lat = location["latitude"]
-        lon = location["longitude"]
-        
-        # now we have finished building the url for the geo weather api , we call it
-        weather_url = WEATHER_BASE_URL
-        params = {
-            "latitude": lat,
-            "longitude": lon,
-            "current_weather" : "true"
-        }
-        weather_res = await client.get(weather_url,params=params)
-        weather_data = weather_res.json()
-        
-        return {
-            "city": location["name"],
-            "latitude": lat,
-            "longitude": lon,
-            "elevation": location["elevation"],
-            "tempurature": weather_data["current_weather"]["temperature"]
-        }
-
-"""
-
-
-""""
-response type for weather data from the open-meteo API
-
-{"results":
-[{"id":2643743,"name":"London","latitude":51.50853,"longitude":-0.12574,
-"elevation":25.0,"feature_code":"PPLC","country_code":"GB","admin1_id":6269131,
-"admin2_id":2648110,"timezone":"Europe/London","population":8961989,
-"country_id":2635167,"country":"United Kingdom","admin1":"England",
-"admin2":"Greater London"}],"generationtime_ms":0.8420944}                                                                                
-from the following curl request : 
-
- curl -i "https://geocoding-api.open-meteo.com/v1/search?
- name=London&count=1&language=en&format=json"
-"""
